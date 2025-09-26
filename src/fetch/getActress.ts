b42d13697bf18fa6979ea58a1d5cbfd2cffc18d2
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-09-26 10:42:45 +0200

    milestone3

---
+++ src/fetch/getActress.ts
@@ -0,0 +1,9 @@
+async function getActress(id){
+    Promise
+    try{
+        const response = await
+    }
+    catch(error){
+        throw new Error(error)
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ type Person = {
 };
 
 type Actress = Person & {
-  most_famous_movie: string[];
+  most_famous_movies: [string, string, string];
   awards: string;
   nationality:
     | "American"
@@ -23,3 +23,48 @@ type Actress = Person & {
     | " South Korean"
     | "Chinese";
 };
+
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === "object" &&
+    dati !== null &&
+    "id" in dati &&
+    typeof dati.id === "number" &&
+    "name" in dati &&
+    typeof dati.name === "string" &&
+    "birth_year" in dati &&
+    typeof dati.birth_year === "number" &&
+    "death_year" in dati &&
+    typeof dati.death_year === "number" &&
+    "biography" in dati &&
+    typeof dati.biography === "string" &&
+    "image" in dati &&
+    typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every((m) => typeof m === "string") &&
+    "awards" in dati &&
+    typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    typeof dati.nationality === "string"
+  );
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const dati: unknown = await response.json();
+    if (!isActress(dati)) {
+      throw new Error(`formato dati non valido`);
+    }
+    return dati;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("errore durante il recupero dell'attorice: ", error);
+    } else {
+      console.error("errore sconosciuto:", error);
+    }
+    return null;
+  }
+}
